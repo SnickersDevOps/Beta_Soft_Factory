39ab9a545de9f6d79534074d111eb4331fac6f29
Timo4ka <82034033+Timo4ka016@users.noreply.github.com>
2022-04-13 11:44:11 +0600

    добавил еще фрагменты

--- Android/unt_front_demo/app/src/main/java/dts/apps/unt_front_demo/fragments/ProfileFragment.kt
+++ Android/unt_front_demo/app/src/main/java/dts/apps/unt_front_demo/fragments/ProfileFragment.kt
@@ -5,15 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import dts.apps.unt_front_demo.databinding.FragmentCoursesBinding
+import dts.apps.unt_front_demo.databinding.FragmentProfileBinding
 
-class CoursesFragment : Fragment() {
-
+class ProfileFragment : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding = FragmentCoursesBinding.inflate(inflater , container , false)
+        val binding = FragmentProfileBinding.inflate(inflater , container , false)
         return binding.root
     }
-
-
-
 }
