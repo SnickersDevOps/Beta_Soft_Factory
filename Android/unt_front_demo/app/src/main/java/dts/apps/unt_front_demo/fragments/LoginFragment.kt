d29e926de461a4a3b4a8722fbfdf21f1c2992b8c
Timo4ka <82034033+Timo4ka016@users.noreply.github.com>
2022-04-15 03:27:52 +0600

    добавил проверку на правильно введенного email адреса

--- Android/unt_front_demo/app/src/main/java/dts/apps/unt_front_demo/fragments/LoginFragment.kt
+++ Android/unt_front_demo/app/src/main/java/dts/apps/unt_front_demo/fragments/LoginFragment.kt
@@ -1,6 +1,8 @@
 package dts.apps.unt_front_demo.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,17 +13,43 @@ import dts.apps.unt_front_demo.R
 import dts.apps.unt_front_demo.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding = FragmentLoginBinding.inflate(inflater , container , false)
-        binding.btnLoginLogin.setOnClickListener {
-            if (binding.editTextEmailLogin.text.toString().isEmpty()) {
-                Toast.makeText(context, "Please enter Email", Toast.LENGTH_SHORT).show()
-            } else if (binding.editTextPasswordLogin.text.toString().isEmpty()) {
-                Toast.makeText(context, "Please enter Password", Toast.LENGTH_SHORT).show()
-            } else {
-                view?.findNavController()?.navigate(R.id.action_loginFragment_to_coursesFragment)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.editTextEmailLogin.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
             }
-        }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                if (binding.editTextEmailLogin.text.toString().isEmpty() or binding.editTextPasswordLogin.text.toString().isEmpty()
+                ) {
+                   binding.editTextPasswordLogin.setError("Invalid Password")
+                }
+                else {
+                    if (android.util.Patterns.EMAIL_ADDRESS.matcher(binding.editTextEmailLogin.text.toString()).matches()
+                    ) {
+                        binding.btnLoginLogin.setOnClickListener {
+                            view?.findNavController()
+                                ?.navigate(R.id.action_loginFragment_to_coursesFragment)
+                        }
+                    } else {
+                        binding.editTextEmailLogin.setError("Invalid Email")
+                    }
+                }
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        })
+
         return binding.root
     }
 }
--- Android/unt_front_demo/app/src/main/java/dts/apps/unt_front_demo/fragments/SignUpFragment.kt
+++ Android/unt_front_demo/app/src/main/java/dts/apps/unt_front_demo/fragments/SignUpFragment.kt
@@ -1,6 +1,8 @@
 package dts.apps.unt_front_demo.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,11 +17,36 @@ class SignUpFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = FragmentSignupBinding.inflate(inflater , container , false)
 
-        binding.btnSignupSignup.setOnClickListener {
-            if (binding.editTextEmailSignup.text.toString().isEmpty()) { Toast.makeText(context , "Please enter Email" , Toast.LENGTH_SHORT).show() }
-            else if (binding.editTextPasswordSignup.text.toString().isEmpty()) { Toast.makeText(context , "Please enter Password" , Toast.LENGTH_SHORT).show() }
-            else { view?.findNavController()?.navigate(R.id.action_signUpFragment_to_loginFragment) }
-        }
+        binding.editTextEmailSignup.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                if (binding.editTextEmailSignup.text.toString().isEmpty() or binding.editTextPasswordSignup.text.toString().isEmpty()
+                ) {
+                    binding.editTextPasswordSignup.setError("Invalid Password")
+                }
+                else {
+                    if (android.util.Patterns.EMAIL_ADDRESS.matcher(binding.editTextEmailSignup.text.toString()).matches()
+                    ) {
+                        binding.btnSignupSignup.setOnClickListener {
+                            view?.findNavController()
+                                ?.navigate(R.id.action_signUpFragment_to_loginFragment)
+                        }
+                    } else {
+                        binding.editTextEmailSignup.setError("Invalid Email")
+                    }
+                }
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        })
+
         return binding.root
     }
 
